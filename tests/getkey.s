17b105741148e3ebc0bab9a6de3e3306f2c864a5
Viktor Holmgren <Viktor.holmgren@gmail.com>
2015-02-02 20:56:50 +0100

    Implement subroutine getkey in test and corresponding test

---
+++ tests/getkey.s
@@ -0,0 +1,24 @@
+start:
+	move.l #$7000,a7	; set stack pointer
+	jsr getkey			; call getkey
+	move.b #255,d7
+	trap #14
+
+getkey:
+	move.l d0,-(a7)		; throw on stack
+	move.l #0,d0		; reset d0
+waitkey:
+	move.b $10082,d0	; move PIAB tp d+
+	and.b #16,d0		; isolate strobe
+	beq waitkey			; jump if false
+
+relkey:
+	move.b $10082,d0	; move PIAB to d0
+	and.b #16,d0		; isolate strobe
+	bne relkey			; jump if true
+	move.b $10082,d0	; move PIAB to d0
+	and.b #15,d0		; isolate key data
+	move.l d0,d4		; save result in d4
+	
+	move.l (a7)+,d0		; restore from stack
+	rts

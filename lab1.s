7ea9f2376405d81357a76254c5e1a8aa1f86720c
Viktor Holmgren <vikho394@student.liu.se>
2015-02-05 12:55:02 +0100

    Correct indent and improve some wording in comments

--- lab1.s
+++ lab1.s
@@ -10,7 +10,6 @@ start:
 	move.b #$21,$402C
 	move.l #13,d5		; string len
 	move.l #$4020,a4	; string pos
-	
 
 restart:
 	jsr clearinput		; clear input buffer
@@ -24,22 +23,22 @@ waitactivate:
 	jsr activatealarm
 
 	move.l #0,d1		; reset d1
-	move.l #$0,d2		; set d2 to first ASCII numeric
+	move.l #$0,d2		; set d2 to first numeric
 
 waitinput:
 	jsr getkey
 	move.b #10,d1		; init lopp var
 
-checkkey:			; Checks whether key is numeric
+checkkey:				; Checks whether key is numeric
 	cmp.b d2,d4
 	beq addkeyif		; if numeric
-	add.b #1,d2		; next ASCII numeric
-	sub.b #1,d1		; inrement loop var
+	add.b #1,d2			; next numeric
+	sub.b #1,d1			; inrement loop var
 	bne checkkey
-	bra waitforcode		; if false check if F
+	bra waitforcode		; if false check if 'F'
 
 addkeyif:
-	jsr addkey		; add keypressed
+	jsr addkey			; add keypressed
 	bra waitinput		; next key
 
 waitforcode:
@@ -47,8 +46,8 @@ waitforcode:
 	bne waitinput		; incorrect input
 	jsr checkcode		; check if code is correct
 	cmp.b #1,d4
-	beq restart		; jump and deactivate alarm
-	jsr printstring		; write 'Fel kod'
+	beq restart			; jump and deactivate alarm
+	jsr printstring		; write error code
 	bra waitinput		; else wait for next key
 
 	move.b #255,d7
@@ -60,10 +59,10 @@ waitforcode:
 ;;; Setuppia subrountine ;;;
 setuppia:
 	move.b #0,$10084	; choose DDRA
-	move.b #1,$10080	; set PIAA pin 0 to output
+	move.b #1,$10080	; set PIA port A pin 0 to output
 	move.b #4,$10084	; choose I/O registry
 	move.b #0,$10086	; choose DDRB
-	move.b #0,$10082	; set all pins to input
+	move.b #0,$10082	; set all PIA port B pins to input 
 	move.b #4,$10086	; choose I/O registry
 	rts
 
@@ -86,11 +85,11 @@ clearinput:
 getkey:
 waitkey:
 	btst #4,$10082		; check strobe
-	beq waitkey		; jump if not strobe
+	beq waitkey			; jump if not strobe
 
 relkey:
 	btst #4,$10082		; jump if not strobe
-	bne relkey		; jump if strobe
+	bne relkey			; jump if strobe
 	move.b $10082,d4	; move PIAB to d4
 	and.b #15,d4		; isolate key data
 	rts
@@ -105,6 +104,7 @@ addkeyloop:
 	move.b 1(a0),(a0)+	; shift a0 + 1 to a0
 	sub.b #1,d0
 	bne addkeyloop
+
 	move.b d1,$4003		; move in new value
 	move.l (a7)+,d0		; restore from stack
 	move.l (a7)+,a0		; restore from stack
@@ -112,15 +112,15 @@ addkeyloop:
 	
 ;;; Checkcode subroutine ;;;
 checkcode:
-	move.l #1,d4		; set default val
+	move.l #1,d4		; set default value
 	move.l d5,-(a7)		; throw on stack
 	move.l d6,-(a7)		; throw on stack
 	move.l $4000,d5		; point to input code
 	move.l $4010,d6		; point to stored code
 	
-	cmp.l d5,d6		; compare pointers
+	cmp.l d5,d6			; compare pointers
 	bne falsecode		; false, jump
-	bra endcode		; jump to end
+	bra endcode			; jump to end
 falsecode:
 	move.l #0,d4		; set d4 (false)
 endcode:
@@ -136,7 +136,7 @@ printstring:
 printloop:
 	move.b (a4)+,d4		; a4 value to d4 and increment
 	jsr printchar		; call printchar
-	sub.b #1,d5		; increment loop var
+	sub.b #1,d5			; increment loop var
 	bne printloop		; loop call
 	
 	move.b #$0A,d4		; new line
@@ -148,12 +148,11 @@ printloop:
 
 ;;; Printchar subroutine ;;;
 printchar:
-	move.b d5,-(a7)         ; save d5 on stack
+	move.b d5,-(a7)     ; save d5 on stack
 waittx:
-
-	move.b $10040,d5        ; status register for serialport
-	and.b #2,d5		; isolate bit 1, ready for transmit
-	beq waittx		; wait until serialport is ready to transmit
+	move.b $10040,d5    ; status register for serialport
+	and.b #2,d5			; isolate bit 1, ready for transmit
+	beq waittx			; wait until serialport is ready to transmit
 	move.b d4,$10042 	; transmit d4
 	move.b (a7)+,d5	 	; reset d5
 	rts

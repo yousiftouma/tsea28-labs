62ef0d84966c6dc143cfacb5910c680c5e2c95aa
Viktor Holmgren <vikho394@student.liu.se>
2015-02-05 10:18:25 +0100

    Extra task 9.2

---
+++ lab1-92.s
@@ -0,0 +1,181 @@
+
+start:
+	move.l #$7000,a7	; set stack pointer
+	jsr setuppia		; setup PIAA, PIAB
+
+	; 'Felaktig kod!' ;
+	move.l #$46656C61,$4020
+	move.l #$6B746967,$4024
+	move.l #$206B6F64,$4028
+	move.b #$21,$402C
+	move.l #13,d5		; string len
+	move.l #$4020,a4	; string pos
+	
+restart:
+	jsr clearinput		; clear input buffer
+	jsr deactivatealarm
+
+waitactivate:
+	jsr Getkeywithcounter
+	cmp.b #$A,d4		; wait for A to be pressed
+	bne waitactivate
+
+	jsr activatealarm
+
+	move.l #0,d1		; reset d1
+	move.l #$0,d2		; set d2 to first ASCII numeric
+
+waitinput:
+	jsr getkey
+	move.b #10,d1		; init lopp var
+
+checkkey:			; Checks whether key is numeric
+	cmp.b d2,d4
+	beq addkeyif		; if numeric
+	add.b #1,d2		; next ASCII numeric
+	sub.b #1,d1		; inrement loop var
+	bne checkkey
+	bra waitforcode		; if false check if F
+
+addkeyif:
+	jsr addkey		; add keypressed
+	bra waitinput		; next key
+
+waitforcode:
+	cmp.b #$F,d4		; wait fpr F tp ne pressed
+	bne waitinput		; incorrect input
+	jsr checkcode		; check if code is correct
+	cmp.b #1,d4
+	beq restart		; jump and deactivate alarm
+	jsr printstring		; write 'Fel kod'
+	bra waitinput		; else wait for next key
+
+	move.b #255,d7
+	trap #14
+
+
+;;; ;;; Subroutines ;;; ;;;
+
+;;; Setuppia subrountine ;;;
+setuppia:
+	move.b #0,$10084	; choose DDRA
+	move.b #1,$10080	; set PIAA pin 0 to output
+	move.b #4,$10084	; choose I/O registry
+	move.b #0,$10086	; choose DDRB
+	move.b #0,$10082	; set all pins to input
+	move.b #4,$10086	; choose I/O registry
+	rts
+
+;;; Activatealarm subroutine ;;;
+activatealarm:
+	move.b #1,$10080	; set PIAA pin 0 to 1
+	rts
+
+;;; Deactivatealarm subroutine ;;;
+deactivatealarm:
+	move.b #0,$10080	; set PIAA pin 0 to 0
+	rts
+
+;;; Clearinput subroutine ;;;
+clearinput:
+	move.l #$FFFFFFFF,$4000	; reset $4000
+	rts
+
+;;; Getkey subroutine ;;;
+getkey:
+waitkey:
+	btst #4,$10082		; check strobe
+	beq waitkey		; jump if not strobe
+
+relkey:
+	btst #4,$10082		; jump if not strobe
+	bne relkey		; jump if strobe
+	move.b $10082,d4	; move PIAB to d4
+	and.b #15,d4		; isolate key data
+	rts
+
+;;; Getkeywithcounter subroutine ;;;
+Getkeywithcounter:
+	move.l d0,-(a7)		; throw on stack
+	move.l #1000000,d0	; init loop var
+waitkeycounter:
+	sub.l #1,d0			; decrement loop var
+	beq endgetkeycounter
+
+	btst #4,$10082		; check strobe
+	beq waitkeycounter	; loop if not pressed
+
+relkeycounter:
+	btst #4,$10082		; check strobe
+	bne relkeycounter	; loop if still pressed
+	move.b $10082,d4	; move PIAB to d4
+	and.b #15,d4		; isolate key data
+	rts
+
+endgetkeycounter:
+	move.b #$A,d4		; fake key to deactivate alarm
+	move.l (a7)+,d0		; restore from stack
+	rts
+
+;;; Addkey subroutine ;;;
+addkey:
+	move.l a0,-(a7)		; throw on stack
+	move.b d0,-(a7)		; throw on stack
+	move.l $4000,a0		; oldest key
+	move.b #2,d0		; init loop var
+addkeyloop:
+	move.b 1(a0),(a0)+	; shift a0 + 1 to a0
+	sub.b #1,d0
+	bne addkeyloop
+	move.b d1,$4003		; move in new value
+	move.l (a7)+,d0		; restore from stack
+	move.l (a7)+,a0		; restore from stack
+	rts
+	
+;;; Checkcode subroutine ;;;
+checkcode:
+	move.l #1,d4		; set default val
+	move.l d5,-(a7)		; throw on stack
+	move.l d6,-(a7)		; throw on stack
+	move.l $4000,d5		; point to input code
+	move.l $4010,d6		; point to stored code
+	
+	cmp.l d5,d6		; compare pointers
+	bne falsecode		; false, jump
+	bra endcode		; jump to end
+falsecode:
+	move.l #0,d4		; set d4 (false)
+endcode:
+	move.l (a7)+,d6		; restore from stack
+	move.l (a7)+,d5		; restore from stack
+	rts
+
+;;; Printstring subroutine ;;;
+printstring:
+	move.l d4,-(a7)		; throw on stack
+	move.l a4,-(a7)		; throw on stack
+	move.l d5,-(a7)		; throw on stack
+printloop:
+	move.b (a4)+,d4		; a4 value to d4 and increment
+	jsr printchar		; call printchar
+	sub.b #1,d5		; increment loop var
+	bne printloop		; loop call
+	
+	move.b #$0A,d4		; new line
+	jsr printchar
+	move.l (a7)+,d5		; restore from stack
+	move.l (a7)+,a4		; restore from stack
+	move.l (a7)+,d4		; restore from stack
+	rts
+
+;;; Printchar subroutine ;;;
+printchar:
+	move.b d5,-(a7)         ; save d5 on stack
+waittx:
+
+	move.b $10040,d5        ; status register for serialport
+	and.b #2,d5		; isolate bit 1, ready for transmit
+	beq waittx		; wait until serialport is ready to transmit
+	move.b d4,$10042 	; transmit d4
+	move.b (a7)+,d5	 	; reset d5
+	rts
